8bd1c9a16108abf82b8a8655b92307afe161c17e
Xavier Lermusieaux <xavier.lermusieaux@gmail.com>
2026-02-24 11:54:42 -0500

    wip: evaluate

--- Partie2/internal/algo/McVitieWilson.go
+++ Partie2/internal/algo/McVitieWilson.go
@@ -30,4 +30,48 @@ func offer(rid int, residents map[int]*csv.Resident, programs map[string]*csv.Pr
 
 func evaluate(resident *csv.Resident, program *csv.Program, residents map[int]*csv.Resident, programs map[string]*csv.Program) {
 
+	if !in(resident.ResidentID, program.Rol) {
+		offer(resident.ResidentID, residents, programs)
+	} else if program.NPositions != len(program.SelectedResidents) {
+		resident.MatchedProgram = program.ProgramID
+		program.SelectedResidents = append(program.SelectedResidents, resident)
+	} else if rPrime, ok := prefers(program, resident); ok {
+		program.SelectedResidents = program.SelectedResidents[len(program.SelectedResidents)-1:] // Remove r' from the selected residents
+		program.SelectedResidents = append(program.SelectedResidents, resident)
+		resident.MatchedProgram = program.ProgramID
+		offer(rPrime.ResidentID, residents, programs)
+	} else {
+		offer(resident.ResidentID, residents, programs)
+	}
+}
+
+func in(val int, list []int) bool {
+	for v := range list {
+		if val == v {
+			return true
+		}
+	}
+	return false
+}
+
+func prefers(p *csv.Program, r *csv.Resident) (*csv.Resident, bool) {
+	rPrime := p.SelectedResidents[len(p.SelectedResidents)-1]
+	var (
+		rPrimeRank int
+		rRank      int
+	)
+	for currentRes, idx := range p.Rol {
+		if currentRes == rPrime.ResidentID {
+			rPrimeRank = idx
+		}
+		if currentRes == r.ResidentID {
+			rRank = idx
+		}
+	}
+
+	if rPrimeRank < rRank {
+		return rPrime, true
+	} else {
+		return nil, false
+	}
 }
